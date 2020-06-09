f577cc82a8a99804ec73467bcc6a956f5c98e204
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-09 19:54:39 +0530

    Add files via upload

---
+++ InterfacePractice.java
@@ -0,0 +1,34 @@
+
+package interfacepractice;
+
+interface Test
+{
+    final static int X=10;
+    public abstract void meth1();
+    public abstract void meth2();
+    public static void meth3()
+    {
+        System.out.println("Meth3 of Test");
+    }
+}
+
+interface Test2 extends Test
+{
+    void meth4();
+}
+
+class My implements Test2
+{
+    public void meth1(){}
+    public void meth2(){}
+    public void meth4(){}
+}
+public class InterfacePractice 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println(Test.X);
+        Test.meth3();
+        
+    }
+}
