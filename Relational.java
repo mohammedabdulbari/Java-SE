b70d0b00cc5e96bc35a24a1cab535fa32444b6c8
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-09 23:59:57 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,24 @@
+
+package relational;
+
+public class Relational 
+{
+    public static void main(String[] args) 
+    {
+        int a=5,b=15,c=4;
+        
+        if(a>b && a>c)
+        {
+            System.out.println(a);
+        }
+        else if(b>c)
+        {
+            System.out.println(b);
+        }
+        else
+        {
+            System.out.println(c);
+        }
+        
+    }   
+}
