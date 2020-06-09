2ad96ab69d5556c21ffcc09cea9802e4a8552617
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-09 23:52:11 +0530

    Update PropertyDemo.java

--- PropertyDemo.java
+++ PropertyDemo.java
@@ -9,14 +9,14 @@ public class PropertyDemo
     {
         Properties p=new Properties();
         
-        /*p.setProperty("Brand", "Dell");
+        p.setProperty("Brand", "Dell");
         p.setProperty("Processor", "i7");
         p.setProperty("OS", "Windows10");
         p.setProperty("Model", "Latitude");
         
         
         p.storeToXML(new FileOutputStream("/Users/abdulbari/Documents/MyData.xml"), "Laptop");
-        */
+        
         
         p.load(new FileInputStream("/Users/abdulbari/Documents/MyData.txt"));
         
