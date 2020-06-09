f2082493fda1abf773ffa2571c07ff94301354a9
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-09 19:15:01 +0530

    Add files via upload

---
+++ FileHandling.java
@@ -0,0 +1,14 @@
+package filehandling;
+import java.io.*;
+
+public class FileHandling 
+{
+    public static void main(String[] args) throws Exception
+    {
+       File f=new File("C:\\MyJava\\Data.txt");
+       
+       FileOutputStream fos =new FileOutputStream("C:\\MyJava\\Data.txt");
+       
+      
+    }
+}
