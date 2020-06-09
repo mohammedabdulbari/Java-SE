8c531655d33e31e243040b92b6af2e6455369474
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-09 15:06:21 +0530

    Add files via upload

---
+++ BufferedDemo.java
@@ -0,0 +1,26 @@
+package buffereddemo;
+import java.io.*;
+
+public class BufferedDemo 
+{
+    public static void main(String[] args)throws Exception 
+    {
+        FileReader fis=new FileReader("C:/MyJava/Test.txt");
+        BufferedReader bis=new BufferedReader(fis);
+        
+        System.out.print((char)bis.read());
+        System.out.print((char)bis.read());
+        System.out.print((char)bis.read());
+        bis.mark(10);
+        System.out.print((char)bis.read());
+        System.out.print((char)bis.read());
+        bis.reset();
+        
+        System.out.print((char)bis.read());
+        System.out.print((char)bis.read());
+        
+        System.out.println("String "+bis.readLine());
+        
+        
+    }    
+}
