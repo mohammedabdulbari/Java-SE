22efb0df0b9856ef3c16385a1d1851df49ffc980
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-09 23:14:08 +0530

    Add files via upload

---
+++ NestedLoops.java
@@ -0,0 +1,19 @@
+package nestedloops;
+
+public class NestedLoops 
+{
+
+    public static void main(String[] args) 
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=7;j++)
+            {
+                System.out.print("("+i+","+j+") ");
+                
+            }
+            System.out.println("");
+        }
+    
+    }   
+}
