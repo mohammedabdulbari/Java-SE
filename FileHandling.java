5466780458c481db50b88f2270e255a977ea7838
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-09 19:18:58 +0530

    Update FileHandling.java

--- FileHandling.java
+++ FileHandling.java
@@ -5,10 +5,17 @@ public class FileHandling
 {
     public static void main(String[] args) throws Exception
     {
-       File f=new File("C:\\MyJava\\Data.txt");
+        // select path of any directory on you computer
+       File f=new File("C:\\MyJava");
        
+       System.out.println(f.isDirectory());
+       File list[]=f.listFiles();
+        
        FileOutputStream fos =new FileOutputStream("C:\\MyJava\\Data.txt");
-       
+       for(File x:list)
+       {
+           System.out.println(x.getParent()+" "+x.getName());
+       }
       
     }
 }
