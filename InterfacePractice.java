3271a1893d5d3d5d2a48af1078d9633382849f05
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-09 19:55:47 +0530

    Update InterfacePractice.java

--- InterfacePractice.java
+++ InterfacePractice.java
@@ -19,9 +19,9 @@ interface Test2 extends Test
 
 class My implements Test2
 {
-    public void meth1(){}
-    public void meth2(){}
-    public void meth4(){}
+    public void meth1(){System.out.println("Meth1");}
+    public void meth2(){System.out.println("Meth2");}
+    public void meth4(){System.out.println("Meth4");}
 }
 public class InterfacePractice 
 {
