9205eb838e3b4057c5483098f4ae76662445b4e0
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-09 23:51:49 +0530

    Add files via upload

---
+++ PropertyDemo.java
@@ -0,0 +1,26 @@
+package propertydemo;
+
+import java.util.*;
+import java.io.*;
+
+public class PropertyDemo 
+{
+    public static void main(String[] args) throws Exception
+    {
+        Properties p=new Properties();
+        
+        /*p.setProperty("Brand", "Dell");
+        p.setProperty("Processor", "i7");
+        p.setProperty("OS", "Windows10");
+        p.setProperty("Model", "Latitude");
+        
+        
+        p.storeToXML(new FileOutputStream("/Users/abdulbari/Documents/MyData.xml"), "Laptop");
+        */
+        
+        p.load(new FileInputStream("/Users/abdulbari/Documents/MyData.txt"));
+        
+        System.out.println(p);
+               
+    }  
+}
