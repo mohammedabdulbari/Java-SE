a76b126bac719e06e54e4dfc3a6a47de7897cb4e
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-09 19:23:06 +0530

    Add files via upload

---
+++ FinallyDemo.java
@@ -0,0 +1,25 @@
+package finallydemo;
+
+public class FinallyDemo 
+{
+    static void meth1()throws Exception
+    {
+        try
+        {
+            
+        throw new Exception();
+        }
+        catch
+                
+        finally
+        {
+        System.out.println("Final Message");
+        }
+        
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        meth1();
+    }   
+}
