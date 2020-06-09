2930484a62290fc0d33362282f593eace318962f
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-09 23:45:19 +0530

    Add files via upload

---
+++ PracticeSwitch.java
@@ -0,0 +1,23 @@
+
+package practiceswitch;
+
+public class PracticeSwitch 
+{
+
+    public static void main(String[] args) 
+    {
+        String str="2";
+        
+        switch(str)
+        {
+            case "1": System.out.println("One");
+                    break;
+            case "2": System.out.println("Two");
+                    break;
+            case "3": System.out.println("Three");
+                    break;
+            default : System.out.println("Invalid");       
+        }
+    }
+    
+}
