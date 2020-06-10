b89b80e6626694cc297b5d17453fc640c0215c42
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-10 14:03:09 +0530

    Add files via upload

---
+++ TokenizerDemo.java
@@ -0,0 +1,36 @@
+package tokenizerdemo;
+
+import java.util.*;
+import java.io.*;
+
+public class TokenizerDemo {
+
+    public static void main(String[] args) throws Exception
+    {
+        
+        FileInputStream fis=new FileInputStream("/Users/abdulbari/Documents/Data.txt");
+        byte b[]=new byte[fis.available()];
+        fis.read(b);
+        
+        String data=new String(b);
+
+        //String data="name=Vijay address=delhi country=india dept=cse";
+    
+        
+        StringTokenizer stk=new StringTokenizer(data,",");
+        
+        String s;
+        ArrayList<Integer> al=new ArrayList<>();
+        
+        while(stk.hasMoreTokens())
+        {
+            s=stk.nextToken();
+            al.add(Integer.valueOf(s));
+            
+        }
+        
+        System.out.println(al);
+
+    }
+    
+}
