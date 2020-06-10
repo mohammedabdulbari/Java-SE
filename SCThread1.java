723834c24c84f011c4b90c2b409abd9b1f265934
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-10 12:46:28 +0530

    Add files via upload

---
+++ SCThread1.java
@@ -0,0 +1,62 @@
+package scthread1;
+
+
+class ATM
+{
+    
+    synchronized public void checkBalance(String name)
+    {
+       System.out.print(name + " Checking ");
+       
+       try{Thread.sleep(1000);}catch(Exception e){}
+       
+       System.out.println("Balance");
+    }
+    
+    synchronized public void withdraw(String name,int amount)
+    {
+        System.out.print(name + " withdrawing "); 
+        
+        try{Thread.sleep(1000);}catch(Exception e){}
+        
+        System.out.println(amount);
+        
+    }
+}
+
+class Customer extends Thread
+{
+    String name;
+    int amount;
+    ATM atm;
+    
+    Customer(String n,ATM a,int amt)
+    {
+        name=n;
+        atm=a;
+        amount=amt;
+    }
+    public void useATM()
+    { 
+        atm.checkBalance(name);
+        atm.withdraw(name, amount);
+    }
+    public void run()
+    {
+        useATM();
+    }
+}
+
+public class SCThread1 
+{
+    public static void main(String[] args) 
+    {
+        ATM atm=new ATM();
+        Customer c1=new Customer("Smith",atm,100);
+        Customer c2=new Customer("John",atm,200);
+        c1.start();
+        c2.start();
+        
+        
+    }   
+}
