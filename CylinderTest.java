73086b1b969131307fb95c922b4be3cfc84c888b
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-15 16:46:17 +0530

    Add files via upload

--- CylinderTest.java
+++ CylinderTest.java
@@ -1,22 +1,50 @@
-
 package cylindertest;
 
-class Cylinder
+class  Cylinder
 {
-    public double radius;
-    public double height;
+    private int radius;
+    private int height;
+    
+    public int getHeight()
+    {
+        return height;
+    }
+    public int getRadius()
+    {
+        return radius;
+    }
+    
+    public void setHeight(int h)
+    {
+        if(h>=0)
+            height=h;
+        else
+            height=0;
+    }
+    public void setRadius(int r)
+    {
+        if(r>=0)
+            radius=r;
+        else
+            radius=0;
+    }
+    public void setDimensions(int h,int r)
+    {
+        height=h;
+        radius=r;
+    }
     
     public double lidArea()
     {
         return Math.PI*radius*radius;
     }
-    public double totalSurfaceArea()
-    {
-        return 2*lidArea()+circumference()*height;
-    }
-    public double circumference()
+    public double perimeter()
     {
         return 2*Math.PI*radius;
+    }
+    public double drumArea()
+    {
+        return 2*lidArea()+perimeter()*height;
     }
     public double volume()
     {
@@ -24,18 +52,22 @@ class Cylinder
     }
 }
 
-public class CylinderTest 
-{
 
-    public static void main(String[] args) 
-    {
+public class CylinderTest {
+
+    public static void main(String[] args) {
         Cylinder c=new Cylinder();
-        c.radius=7;
-        c.height=10;
+        c.setHeight(10);
+        c.setRadius(7);
+        c.setDimensions(10, 7);
         
-        System.out.println("Lid Area: "+c.lidArea());
-        System.out.println("Total Area :"+c.totalSurfaceArea());
+        System.out.println("LidArea "+c.lidArea());
+        System.out.println("Circumference "+c.perimeter());
+        System.out.println("totalSurfaceArea "+c.drumArea());
         System.out.println("Volume "+c.volume());
+        System.out.println("Height"+c.getHeight());
+        System.out.println("Radius"+c.getRadius());
+        
     }
     
 }
