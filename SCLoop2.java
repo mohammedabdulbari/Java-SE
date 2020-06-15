210ff170eddb49d0c3f20a077f92c5cac1163ede
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-15 13:39:42 +0530

    Add files via upload

--- SCLoop2.java
+++ SCLoop2.java
@@ -1,129 +1,76 @@
+
 package scloop2;
 
+import java.lang.*;
 import java.util.*;
+public class SCLoop2 {
 
-public class SCLoop2 
-{
+    /*Display Digits of number
     
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
-        Scanner scan=new Scanner(System.in);
-        
-        System.out.println("Enter a Number");
-        int n=scan.nextInt();
-        int m=n;
-        int rev=0,r;
-        
-        while(n>0)
-        {
-            r=n%10;
-            rev=rev*10+r;
-            n/=10;
-        }
-        if(rev==m)
-            System.out.println("Its is a Palindrome");
-        else
-            System.out.println("Its is not a Palindrome");
-            
-        
-        
-    }
-    
-    
-    
-    /* Reverse of a number
-    
-    
-     public static void main(String[] args) 
-    {
-        Scanner scan=new Scanner(System.in);
-        
-        System.out.println("Enter a Number");
-        int n=scan.nextInt();
-  
-        int rev=0,r;
-        
-        while(n>0)
-        {
-            r=n%10;
-            rev=rev*10+r;
-            n/=10;
-        }
-        
-        System.out.println("Reverse Number "+rev);
-        
-        
-    }
-    
+       Scanner scan=new Scanner(System.in);
 
+       System.out.println("Enter a Number");
+       int n=scan.nextInt();
+
+       int r;
+       while(n>0)
+       {
+        r=n%10;
+        n=n/10;
+
+        System.out.println(r);
+       }
+        System.out.println(n);
+    }
+*/
+    
+    
+    /* Count Digits Of a Number
+    
+    public static void main(String[] args)
+    {
+       Scanner scan=new Scanner(System.in);
+
+       System.out.println("Enter a Number");
+       int n=scan.nextInt();
+
+       int count=0;
+       while(n>0)
+       {
+        n=n/10;
+        count++;
+       }
+        System.out.println(count);
+    }
     */
     
-    /* Check if a Numbewr is Armstrong or Not
-    public static void main(String[] args) 
+   
+    /* armstrong number*/
+    
+    public static void main(String[] args)
     {
-        Scanner scan=new Scanner(System.in);
+       Scanner scan=new Scanner(System.in);
+
+       System.out.println("Enter a Number");
+       int n=scan.nextInt();
+       
+       int m=n;
+       int sum=0;
+       int r;
+       while(n>0)
+       {
+        r=n%10;
+        n=n/10;
         
-        System.out.println("Enter a Number");
-        int n=scan.nextInt();
-        
-        int m=n;
-        int sum=0;
-        int r;
-        
-        while(n>0)
-        {
-            r=n%10;
-            n=n/10;
-            
-            sum=sum+r*r*r;
-        }
+        sum=sum+r*r*r;
+       }
         if(sum==m)
             System.out.println("Its a Armstrong Number");
         else
-            System.out.println("Its Not a Armstrong Number");
-    }   */
+            System.out.println("Its not an Armsttrong Number");
+        
+    }
     
-    /* Count the Number of Digits in a Number
-    public static void main(String[] args) 
-    {
-        Scanner scan=new Scanner(System.in);
-        
-        System.out.println("Enter a Number");
-        int n=scan.nextInt();
-        
-        int count=0;
-        
-        while(n>0)
-        {
-            n=n/10;
-            
-            count++;
-        }
-        
-        System.out.println(count);
-        
-    }   
-    */
-    
-    /* Display digits of a Number
-    public static void main(String[] args) 
-    {
-        Scanner scan=new Scanner(System.in);
-        
-        System.out.println("Enter a Number");
-        int n=scan.nextInt();
-        
-        int r;
-        
-        while(n>0)
-        {
-            r=n%10;
-            n=n/10;
-            
-            System.out.println(r);
-        }
-        
-            
-    }   
-    */
 }
