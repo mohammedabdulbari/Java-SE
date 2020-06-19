4730e248b5e133780c05fa8bbe6db149e4179b66
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-19 11:10:42 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,17 @@
+package first;
+
+import java.util.*;
+
+public class First {
+
+    public static void main(String[] args) {
+        String name;
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("May I know your Name: ");
+        name=sc.nextLine();
+        
+        System.out.println("Welcome "+name);
+    }
+    
+}
