43adfa5437d38521abdc7ffe8ad0cd5a10e90b64
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-21 18:40:49 +0530

    Add files via upload

---
+++ DataSizeRange.java
@@ -0,0 +1,20 @@
+package datasizerange;
+
+public class DataSizeRange {
+
+    public static void main(String[] args) {
+        
+        /*System.out.println("Int Min: "+Integer.MIN_VALUE);
+        System.out.println("Int Max: "+Integer.MAX_VALUE);
+        System.out.println("Int Bytes :"+Integer.BYTES);*/
+        
+        /*System.out.println("Byte Min: "+Byte.MIN_VALUE);
+        System.out.println("Byte Max: "+Byte.MAX_VALUE);
+        System.out.println("Byte Bytes :"+Byte.BYTES);*/
+        
+        System.out.println("FLoat Min: "+Float.MIN_VALUE);
+        System.out.println("Float Max: "+Float.MAX_VALUE);
+        System.out.println("Float Bytes :"+Float.BYTES);
+    }
+    
+}
