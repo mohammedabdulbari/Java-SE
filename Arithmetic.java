095b1944e4d7ec3b2cb6d325fc26e9731e679b2b
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-21 18:53:37 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,53 @@
+package arithematic;
+
+public class Arithematic {
+
+    public static void main(String[] args) {
+        
+        /* for integer values
+        int a=14,b=5;
+        
+        int c=a/b;
+        int r=a%b;
+        
+        System.out.println(c);
+        System.out.println(r);*/
+        
+        
+        
+        /*for float values
+        float a=14.3f,b=3.2f;
+         
+        float c=a/b;
+        float r=a%b;
+        
+        System.out.println(c);
+        System.out.println(r);*/
+        
+        
+        
+        /* for different data types
+        byte a=10;
+        short b=15;
+        int c=a+b;
+        
+        float a=12.5f;
+        long b=1231;
+        float c=a*b;
+        
+        float a=12.5f;
+        double b=123;
+        double c=a*b;
+        
+        char a=40;
+        int b=30;
+        int c=a-b;
+        System.out.println(c);
+        */
+        
+        
+        System.out.println((10+20)/2);
+        System.out.println(10/(2*5));
+    }
+    
+}
