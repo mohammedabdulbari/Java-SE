97a109777f5f24d3c4bbba060e7df36854998de7
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-21 18:47:48 +0530

    Add files via upload

---
+++ DataTypeDetails.java
@@ -0,0 +1,16 @@
+package datatypedetails;
+
+import java.awt.*;
+import javax.swing.*;
+import java.lang.*;
+
+
+public class DataTypeDetails {
+
+    public static void main(String[] args) {
+        int x=-5;//5
+        
+        System.out.println(Integer.toBinaryString(x));
+    }
+    
+}
