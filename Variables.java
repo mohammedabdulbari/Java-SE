bebc39203f241396dc79acc66f96069062c87484
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-21 18:42:22 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        
+        byte b=5;
+        short s=300;
+        int i=100;
+        float f=25.3f;
+        char c='A';
+        
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(c);
+    }
+    
+}
