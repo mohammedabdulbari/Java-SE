7bd215bccec47cd4d711e98fca6c38a322f152dd
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-21 18:46:32 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,25 @@
+package literals;
+
+public class Literals {
+
+    public static void main(String[] args) {
+        /*byte b1=10;
+        byte b2=0b1010;
+        byte b3=012;
+        byte b4=0XA;
+        
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);*/
+        
+        long l=9999999999L;
+        float f=12.56f;
+        double d=12.56d;
+        
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+    }
+    
+}
