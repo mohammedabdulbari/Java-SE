f09ad735b9779cad43227d775aa4f8940eb99abb
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-21 18:45:24 +0530

    Add files via upload

---
+++ VariableNames.java
@@ -0,0 +1,15 @@
+package variablenames;
+
+public class VariableNames {
+
+    public static void main(String[] args) {
+        //int x;
+        //int roll_number$student;
+        //int _x;
+        
+        //int rollNumberOfStudent;
+        //float averageMarksOfClass;
+        
+    }
+    
+}
