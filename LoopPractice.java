072eb67d0af8e7639606bc1b5b775c62c3c3c454
mohammedabdulbari <mohammed.bari@gmail.com>
2020-06-23 18:52:01 +0530

    Add files via upload

---
+++ LoopPractice.java
@@ -0,0 +1,38 @@
+package looppractice;
+
+public class LoopPractice {
+
+    public static void main(String[] args) {
+        
+        //int i=1;//100
+        /*while(i<100)
+        {
+            System.out.println(i);
+            i=i*2;
+        }*/
+        
+        /*do
+        {
+            System.out.println(i);
+            i=i*2;
+        }while(i<100);*/
+        
+        /*byte i=1;
+        while(true)
+        {
+            System.out.println(i);
+            i++;
+        }*/
+        
+        byte i=1;
+        
+        if(true)
+        {
+            System.out.println(i);
+            i++;
+        }
+        else
+            System.out.println("Bye");
+    }
+    
+}
