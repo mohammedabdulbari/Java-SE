b2da511ad11511bece664cf3c71b3070dd34501d
Azaharuddin Ansari <89899589+gLitch21@users.noreply.github.com>
2021-10-14 17:29:19 +0530

    Update SCArray3.java

--- SCArray3.java
+++ SCArray3.java
@@ -26,7 +26,7 @@ public class SCArray3
         int A[]={8,6,10,9,2,15,7,13,14,11};
         int B[]=new int[10];
         
-        for(int i=A.length-1,j=0;i>=0;i++,j++)
+        for(int i=A.length-1,j=0;i>=0;i--,j++)
         {
             B[j]=A[i];
         }
